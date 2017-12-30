539f0f020b34c4e1b61af5d0ce90aa9dbbe480b8
Thesoreon <pavels.sus@gmail.com>
2017-12-30 13:24:02 +0100

    - Default template for react
    Contains:
    - Typescript loaders
    - Less loaders
    - React & react-dom
    - webpack server

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+
+ReactDOM.render(
+    <h1>Hello World! Theso! Pls funguj :(</h1>,
+    document.getElementById("example")
+);
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/main.tsx",
+
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            {test: /\.less$/, use: ["style-loader","css-loader", "less-loader"]},
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 8080,
+        stats: "errors-only", //Avoids terminal logs
+        open: true
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Template",
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            template: "./src/index.ejs",
+        })
+    ],
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    /*externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }*/
+
+}
